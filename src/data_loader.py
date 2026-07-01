b1992f294e13cbc81a8b1e7e9cc36e3057d61841
Shokoufeh <anabella@Venessas-MBP.lan>
2026-07-01 12:50:39 -0700

    data_loader

--- src/data_loader.py
+++ src/data_loader.py
@@ -1,3 +1,5 @@
+# Read CSV files
+
 import pandas as pd
 import glob
 # from sklearn.preprocessing import StandardScaler
@@ -6,12 +8,14 @@ files = glob.glob("../data/*.csv")
 dfs=[]
 
 for file in files:
-    # print("=" * 60)
-    # print(file)
-
+   
     df = pd.read_csv(file)
+    print(df.columns)
     df.columns = df.columns.str.strip()
+    print(df["Label"].value_counts())
     dfs.append(df)
 
 data= pd.concat(dfs, ignore_index=True)
+
+
 
