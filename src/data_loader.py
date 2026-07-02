4ce7175ade5e5f5a267c956a78957791047f3ce8
Sophia Na <amitazh@aol.com>
2026-07-01 20:06:06 -0700

    data_loader defined function and path

--- src/data_loader.py
+++ src/data_loader.py
@@ -1,21 +1,35 @@
 # Read CSV files
 
 import pandas as pd
-import glob
-# from sklearn.preprocessing import StandardScaler
+from pathlib import Path
 
-files = glob.glob("../data/*.csv")
-dfs=[]
 
-for file in files:
-   
-    df = pd.read_csv(file)
-    print(df.columns)
-    df.columns = df.columns.str.strip()
-    print(df["Label"].value_counts())
-    dfs.append(df)
+def load_all_csvs(data_dir= "data"):
 
-data= pd.concat(dfs, ignore_index=True)
+    project_root= Path(__file__).resolve().parent.parent
+    data_path= project_root/ data_dir
 
+    files = list(data_path.glob("*.csv"))
+
+    dfs=[]
+
+    for file in files:
+
+        print(f"reading :{file.name}")
+        df = pd.read_csv(file)
+        print(df.columns)
+        df.columns = df.columns.str.strip()
+        print(df["Label"].value_counts())
+        dfs.append(df)
+
+    data= pd.concat(dfs, ignore_index=True)
+    return data
+
+
+if __name__ == "__main__":
+
+    data = load_all_csvs()
+
+    print(data.shape)
 
 
