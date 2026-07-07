e02e3e73680c065832f781e4b03ca1bf341919ac
Shokoufeh <anabella@Venessas-MBP.lan>
2026-07-06 19:11:15 -0700

    modified:   src/data_loader.py  modified:   src/feature_extractor.py

--- src/data_loader.py
+++ src/data_loader.py
@@ -13,16 +13,16 @@ def load_all_csvs(data_dir= "data"):
 
     for file in files:
 
-        print(f"reading :{file.name}")
+        # print(f"reading :{file.name}")
         df = pd.read_csv(file)
-        print(df.columns)
+        # print(df.columns)
         df.columns = df.columns.str.strip()
-        print(df["Label"].value_counts())
+        # print(df["Label"].value_counts())
         dfs.append(df)
 
     data= pd.concat(dfs, ignore_index=True)
-    print("data shape:", data.shape)
-    print("data head", data.head)
+    # print("data shape:", data.shape)
+    # print("data head", data.head)
 
     return data
 
